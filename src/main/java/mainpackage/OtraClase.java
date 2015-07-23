4a48c2eaf4e631846a860d1d66a167c66e9cf319
Alejandro Homes <ahomes@nisum.com>
2015-07-23 15:39:01 -0300

    Initial project Hola Mundo

---
+++ src/main/java/mainpackage/OtraClase.java
@@ -0,0 +1,12 @@
+package mainpackage;
+
+public class OtraClase {
+
+	OtraClase(){
+		
+	}
+	
+	public void print(){
+		System.out.println("Se ha creado otra clase");
+	}
+}
---
+++ src/main/java/mainpackage/mainClass.java
@@ -0,0 +1,16 @@
+package mainpackage;
+/**
+ * 
+ * @author ahomes
+ *
+ */
+public class mainClass {
+	
+	public static void main(String[] args)
+	{
+		
+		System.out.println("Hola mundo !!!!!!" );
+		OtraClase myclass = new OtraClase();
+		myclass.print();
+	}
+}
